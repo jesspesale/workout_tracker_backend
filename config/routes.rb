2e906ae3cb1fade75a1a97bc9c8155f483b93687
Jess Pesale <jess_pesale@aol.com>
2021-09-09 23:26:24 -0400

    added exercise_attributes controller with namespaced folders to match routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
 
   namespace :api do
-    namespace :v1 do
-      resources :exercise_attributes, only [:index]
+    namespace :v1 do;
+      resources :exercise_attributes, only: [:index]
     end
   end
-
+  #   Prefix                     Verb
+  # api_v1_exercise_attributes  GET    
+  #         URL
+  #/api/v1/exercise_attributes(.:format)
+  #         Controller#action
+  # api/v1/exercise_attributes#index
 
 end
