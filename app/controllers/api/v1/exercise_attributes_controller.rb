1c2476d8ae155fb46a980e33cd17497188430b21
Jess Pesale <jess_pesale@aol.com>
2021-09-10 15:20:58 -0400

    added index and create controller actions and routes

--- app/controllers/api/v1/exercise_attributes_controller.rb
+++ app/controllers/api/v1/exercise_attributes_controller.rb
@@ -1,2 +1,26 @@
 class Api::V1::ExerciseAttributesController < ApplicationController
+
+    def index
+        exercise_attributes = ExerciseAttribute.all
+        render json: exercise_attributes
+    end
+
+    def create
+        exercise_attribute = ExerciseAttribute.new(exercise_attributes_params)
+        if exercise_attributes_params
+            render json: exercise_attribute, status: :accepted
+                                        # allows us to send status codes with our fetch req
+        else 
+            render json: {errors: exercise_attribute.errors.full_messages}, status: :unprocessible_entity
+                            # if not saved properly code: 422 (failed validations)
+        end
+    end
+
+
+    private
+
+    def exercise_attributes_params
+        params.require(:exercise_attribute).permit(:category, :date, :duration, :calories, :workout_id)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
 
+  # can look at routes at:
+  # http://127.0.0.1:3000/rails/info/routes
+
   namespace :api do
     namespace :v1 do;
-      resources :exercise_attributes, only: [:index]
+      resources :exercise_attributes, only: [:index, :create]
     end
   end
+
+
   #   Prefix                     Verb
   # api_v1_exercise_attributes  GET    
   #         URL
