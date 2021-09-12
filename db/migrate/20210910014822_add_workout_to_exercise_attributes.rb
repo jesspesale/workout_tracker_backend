da234067a8df88ef0a1cdf8d4eead7d0f90ab03d
Jess Pesale <jess_pesale@aol.com>
2021-09-12 12:10:13 -0400

    fixed schema and re migrated

--- db/migrate/20210910014822_add_workout_to_exercise_attributes.rb
+++ db/migrate/20210910014822_add_workout_to_exercise_attributes.rb
@@ -1,5 +1,6 @@
 class AddWorkoutToExerciseAttributes < ActiveRecord::Migration[6.1]
   def change
-    	add_reference :exercise_attributes, :workout, foreign_key: true
+    	add_reference :exercise_attributes, :workout, null: false, foreign_key: true
+# if was null would have no way to clean out the db and associated items
   end
 end
