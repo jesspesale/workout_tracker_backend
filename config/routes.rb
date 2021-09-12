a3a5f4fd15e64f771a386cbbb1502079da00851b
Jess Pesale <jess_pesale@aol.com>
2021-09-12 12:13:59 -0400

    added index and create routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do;
       resources :exercise_attributes, only: [:index, :create]
+      resources :workouts, only: [:index, :create]
     end
   end
 
