91bfab47b1d2e8ef1eb6eceea4135680960c6d34
Jess Pesale <jess_pesale@aol.com>
2021-09-17 10:08:00 -0400

    tried to mess up something for a catch in a fetch but it didnt work

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,5 +1,7 @@
 class Workout < ApplicationRecord
     has_many :exercise_attributes, dependent: :destroy
     
+    validates :title, :date, presence: true
+    
     accepts_nested_attributes_for :exercise_attributes
 end
